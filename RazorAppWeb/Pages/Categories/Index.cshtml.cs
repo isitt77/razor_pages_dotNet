b17972f24984239cfef701ba0a47527b6f21a744
Scott Isitt <isitts@hotmail.com>
2022-10-16 00:11:58 -0700

    Change Category directory to Categoriies.

---
+++ RazorAppWeb/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorAppWeb.Data;
+using RazorAppWeb.Model;
+
+namespace RazorAppWeb.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        //public IEnumerable<Category> Categories { get; set; }
+
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            IEnumerable<Category> Categories = _db.Categories;
+        }
+    }
+}
