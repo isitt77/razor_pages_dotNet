40ab69dde9b01a5dd197a5d5facf00b50c9fb836
Scott Isitt <isitts@hotmail.com>
2022-10-20 18:23:12 -0700

    Reference Category Model in Create page model. Use asp  helpers for inputs and labels.

--- RazorAppWeb/Model/Category.cs
+++ RazorAppWeb/Model/Category.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace RazorAppWeb.Model
@@ -9,6 +10,8 @@ namespace RazorAppWeb.Model
         public int Id { get; set; }
         [Required]
         public string Name { get; set; } = "";
+        [DisplayName("Display Order")]
+        [Range(1, 100)]
         public int DisplayOrder { get; set; }
     }
 }
--- RazorAppWeb/Pages/Categories/Create.cshtml.cs
+++ RazorAppWeb/Pages/Categories/Create.cshtml.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorAppWeb.Model;
 
 namespace RazorAppWeb.Pages.Categories
 {
     public class CreateModel : PageModel
     {
+        public Category category { get; set; }
+
         public void OnGet()
         {
         }
