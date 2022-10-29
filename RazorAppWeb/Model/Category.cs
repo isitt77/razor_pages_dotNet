1589dfa4cb2ef2e2f5e5dad78141a56b124d85e6
Scott Isitt <isitts@hotmail.com>
2022-10-28 23:02:01 -0700

    Add custom error message to Display Order range attribute in Category Model.

--- RazorAppWeb/Model/Category.cs
+++ RazorAppWeb/Model/Category.cs
@@ -11,7 +11,8 @@ namespace RazorAppWeb.Model
         [Required]
         public string Name { get; set; } = "";
         [DisplayName("Display Order")]
-        [Range(1, 100)]
+        [Range(1, 100, ErrorMessage = "Display Order must be " +
+            "between 1 and 100.")]
         public int DisplayOrder { get; set; }
     }
 }
