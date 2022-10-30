78cf80ef6024027beac2181a28f89386863f01b4
Scott Isitt <isitts@hotmail.com>
2022-10-29 19:19:25 -0700

    Add Edit page with model.

---
+++ RazorAppWeb/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorAppWeb.Data;
+using RazorAppWeb.Model;
+
+namespace RazorAppWeb.Pages.Categories
+{
+    public class EditModel : PageModel
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public Category category { get; set; }
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError(string.Empty, "Name and " +
+                    "Display Order cannot be the same.");
+            }
+            // ModelState is only hit when there are no client side validations.
+            Console.WriteLine("isValidModelState: " + ModelState.IsValid);
+            if (ModelState.IsValid)
+            {
+                await _db.Categories.AddAsync(category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
