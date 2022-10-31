636d0c6dff57c7832e65d3b98d6652657178b494
Scott Isitt <isitts@hotmail.com>
2022-10-30 19:10:20 -0700

    Create Delete page and model. Write logic to remove object.

---
+++ RazorAppWeb/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorAppWeb.Data;
+using RazorAppWeb.Model;
+
+namespace RazorAppWeb.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public Category category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public void OnGet(int id)
+        {
+            category = _db.Categories.Find(id);
+        }
+
+
+        public async Task<IActionResult> OnPost(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}
